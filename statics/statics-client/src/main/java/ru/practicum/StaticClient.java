f3cb8bfcfebd412299a770b240f64116e55c36af
East of Eden <azazal.combs@gmail.com>
2025-03-04 23:35:12 +0300

    Добавил клиент

---
+++ statics/statics-client/src/main/java/ru/practicum/StaticClient.java
@@ -0,0 +1,49 @@
+package ru.practicum;
+
+import lombok.AllArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StaticClient {
+    private final RestTemplate restTemplate;
+
+    private final String BASE_URL = "http://localhost:9090";
+
+    public void createEvent(NewEventRequest newEventRequest) throws Exception {
+        String url = BASE_URL + "/hit";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<NewEventRequest> requestEntity = new HttpEntity<>(newEventRequest, headers);
+
+        ResponseEntity<Void> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Void.class);
+
+        if (response.getStatusCode() != HttpStatus.CREATED) {
+            throw new Exception("Код ответа сервиса не 201");
+        }
+    }
+
+    public List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, Boolean unique, List<String> uris) {
+        String url = String.format("%s/stats?start=%s&end=%s&unique=%s",
+                BASE_URL, start, end, unique);
+
+        if (uris != null && !uris.isEmpty()) {
+            String uriParams = String.join(",", uris);
+            url += "&uris=" + uriParams;
+        }
+
+        ResponseEntity<List<ViewStats>> response = restTemplate.exchange(url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<ViewStats>>() {
+                });
+
+        return response.getBody();
+    }
+}
