ba45f3d96ca961f8ab29a66ed6305a50a1b360a6
East of Eden <azazal.combs@gmail.com>
2025-03-20 20:03:27 +0300

    Поправил точки

--- main-server/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
+++ main-server/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
@@ -33,8 +33,8 @@ public class CategoryServiceImpl implements CategoryService {
             throw new ForbiddenException("Категория " + newCategoryRequest.getName() + " уже используется");
         }
 
-        return CategoryMapper.mapToCategoryDto(categoryRepository.
-                save(CategoryMapper.mapToCategory(newCategoryRequest)));
+        return CategoryMapper.mapToCategoryDto(categoryRepository
+                .save(CategoryMapper.mapToCategory(newCategoryRequest)));
     }
 
     @Override
@@ -53,8 +53,8 @@ public class CategoryServiceImpl implements CategoryService {
         }
 
         //Обновляем название категории
-        return CategoryMapper.mapToCategoryDto(categoryRepository.
-                save(CategoryMapper.mapToCategory(id, updateCategoryRequest)));
+        return CategoryMapper.mapToCategoryDto(categoryRepository
+                .save(CategoryMapper.mapToCategory(id, updateCategoryRequest)));
     }
 
     @Override
